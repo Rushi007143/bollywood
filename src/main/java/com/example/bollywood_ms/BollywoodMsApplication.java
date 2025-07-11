082153a5b5fa447ef0a682575506ea0e9814fdc6
Rushi007143 <rushikeshthul007@gmail.com>
2025-07-11 18:40:58 +0530

    coderefactor

---
+++ src/main/java/com/example/bollywood_ms/BollywoodMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bollywood_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BollywoodMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BollywoodMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bollywood_ms/BollywoodMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bollywood_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BollywoodMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
